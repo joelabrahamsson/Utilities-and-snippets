9a40483cd40eba5ac35cb3a8ebefbb64baccda87
joelabrahamsson <mail@joelabrahamsson.com>
2011-06-02 16:07:25 +0200

    First commit with some basic extension methods.

---
+++ Common/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Common
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull(this object value)
+        {
+            return value == null;
+        }
+
+        public static bool IsNotNull(this object value)
+        {
+            return !value.IsNull();
+        }
+    }
+}
