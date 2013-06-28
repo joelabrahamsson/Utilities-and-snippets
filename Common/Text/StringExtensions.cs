a6d171c45eeba87a3121549a68f2c75d42d2aa0e
Joel Abrahamsson <mail@joelabrahamsson.com>
2013-06-28 16:59:32 +0200

    Method for cropping text without breaking words

--- Common/Text/StringExtensions.cs
+++ Common/Text/StringExtensions.cs
@@ -1,10 +1,156 @@
-﻿namespace Common.Text
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Text
 {
     public static class StringExtensions
     {
         public static bool IsNullOrEmpty(this string value)
         {
             return string.IsNullOrEmpty(value);
+        }
+
+        private static readonly HashSet<char> DefaultNonWordCharacters = new HashSet<char> { ',', '.', ':', ';' };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="length"></param>
+        /// <param name="nonWordCharacters"></param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentException">Thrown when <paramref name="length"/> is negative</exception>
+        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+        public static string CropWholeWords(this string value, int length, HashSet<char> nonWordCharacters = null)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            if (length < 0)
+            {
+                throw new ArgumentException("Negative values not allowed.", "length");
+            }
+
+            if (nonWordCharacters == null)
+            {
+                nonWordCharacters = DefaultNonWordCharacters;
+            }
+
+            //ALT3
+            if (length > value.Length)
+            {
+                length = value.Length;
+            }
+            int end = length;
+
+            bool multipleWords = false;
+            for (int i = end; i > 0; i--)
+            {
+                if (value.Length > i && value[i] == ' ')
+                {
+                    multipleWords = true;
+                }
+
+                if (value.Length == length)
+                {
+                    break;
+                }
+
+                if (value.Length > i && (value[i] == ' ' || (nonWordCharacters.Contains(value[i]) && (value.Length == i + 1 || value[i + 1] == ' '))))
+                {
+                    break;
+                }
+                end--;
+            }
+
+            if (end == 0 && !multipleWords)
+            {
+                end = length;
+            }
+
+            return value.Substring(0, end);
+
+            //ALT2
+            //int end = 0;
+            //if (length > value.Length)
+            //{
+            //    length = value.Length;
+            //}
+            //bool multipleWords = false;
+            //int lastWordEnd = 0;
+            //for (int i = 0; i < length; i++)
+            //{
+            //    end++;
+            //    if (value.Length > length && value[i + 1] == ' ')
+            //    {
+            //        lastWordEnd = end;
+            //        multipleWords = true;
+            //    }
+            //    if (i + 1 == length && value.Length > length && !nonWordCharacters.Contains(value[i + 1]) && multipleWords)
+            //    {
+            //        end = lastWordEnd;
+            //    }
+            //}
+
+            //return value.Substring(0, end);
+
+            //ALT1
+            //if (length > value.Length)
+            //{
+            //    return value;
+            //}
+
+            //if (nonWordCharacters == null)
+            //{
+            //    nonWordCharacters = defaultNonWordCharacters;
+            //}
+
+            //var words = value.Split(new[] {' '});
+            //var buffer = new StringBuilder();
+            //foreach (var word in words)
+            //{
+            //    if (buffer.Length == 0 && word.Length >= length)
+            //    {
+            //        buffer.Append(word.Substring(0, length));
+            //    }
+
+            //    var wordToInsert = word;
+            //    if (buffer.Length + word.Length + 1 > length)
+            //    {
+            //        if (nonWordCharacters.Contains(wordToInsert[wordToInsert.Length-1]) && buffer.Length + wordToInsert.Length <= length)
+            //        {
+            //            wordToInsert = wordToInsert.Substring(0, wordToInsert.Length - 1);
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+
+            //    if (buffer.Length > 0)
+            //    {
+            //        buffer.Append(" ");
+            //    }
+
+            //    buffer.Append(wordToInsert);
+            //}
+            //return buffer.ToString();
+        }
+
+        public static bool EndsWith(this string fullString, HashSet<char> characters)
+        {
+            foreach (var character in characters)
+            {
+                if (fullString[fullString.Length - 1].Equals(character))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
---
+++ Specifications/Text/Cropping.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Diagnostics;
+using Common.Text;
+using Machine.Specifications;
+
+namespace Specifications.Text
+{
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_null
+    {
+        static Exception thrownException;
+
+        Because of
+            = () => thrownException = Catch.Exception(() => ((string)null).CropWholeWords(5));
+
+        It should_thrown_an_ArgumentNullException
+            = () => thrownException.ShouldBeOfType<ArgumentNullException>();
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_an_string_and_a_negative_length
+    {
+        static Exception thrownException;
+
+        Because of
+            = () => thrownException = Catch.Exception(() => ("yellow").CropWholeWords(-5));
+
+        It should_thrown_an_ArgumentException
+            = () => thrownException.ShouldBeOfType<ArgumentException>();
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_an_empty_string_and_a_positive_length
+    {
+        static string input = string.Empty;
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(14);
+
+        It should_return_an_empty_string
+            = () => output.ShouldEqual(string.Empty);
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_and_length_14
+    {
+        static string input = "yellow bananas";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(14);
+
+        It should_return_the_full_string
+            = () => output.ShouldEqual(input);
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_and_length_15
+    {
+        static string input = "yellow bananas";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(15);
+
+        It should_return_the_full_string
+            = () => output.ShouldEqual(input);
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_and_length_6
+    {
+        static string input = "yellow bananas";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(6);
+
+        It should_return_yellow
+            = () => output.ShouldEqual("yellow");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_and_length_7
+    {
+        static string input = "yellow bananas";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(7);
+
+        It should_return_yellow
+            = () => output.ShouldEqual("yellow");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_and_length_8
+    {
+        static string input = "yellow bananas";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(8);
+
+        It should_return_yellow
+            = () => output.ShouldEqual("yellow");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_and_length_5
+    {
+        static string input = "yellow bananas";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(5);
+
+        It should_return_yello
+            = () => output.ShouldEqual("yello");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_I_am_a_cat__and_length_5
+    {
+        static string input = "I am a cat";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(5);
+
+        It should_return_I_am
+            = () => output.ShouldEqual("I am");
+    }
+    
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_comma_and_length_14
+    {
+        static string input = "yellow bananas,";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(14);
+
+        It should_return_yellow_bananas
+            = () => output.ShouldEqual("yellow bananas");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_period_and_length_14
+    {
+        static string input = "yellow bananas.";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(14);
+
+        It should_return_yellow_bananas
+            = () => output.ShouldEqual("yellow bananas");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_colon_and_length_14
+    {
+        static string input = "yellow bananas:";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(14);
+
+        It should_return_yellow_bananas
+            = () => output.ShouldEqual("yellow bananas");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_bananas_semicolon_and_length_14
+    {
+        static string input = "yellow bananas;";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(14);
+
+        It should_return_yellow_bananas
+            = () => output.ShouldEqual("yellow bananas");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_comma_and_length_6
+    {
+        static string input = "yellow,";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(6);
+
+        It should_return_yellow
+            = () => output.ShouldEqual("yellow");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class given_the_string_yellow_period_bananas_and_length_8
+    {
+        static string input = "yellow.bananas";
+        static string output;
+
+        Because of
+            = () => output = input.CropWholeWords(8);
+
+        It should_return_bananas_period_b
+            = () => output.ShouldEqual("yellow.b");
+    }
+
+    [Subject(typeof(StringExtensions), "CropWholeWords")]
+    public class when_invoked_with_a_fairly_long_string_ten_thousand_times
+    {
+        static string input = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
+        static long elapsedMilliseconds;
+
+        Because of
+            = () =>
+                {
+                    var timer = new Stopwatch();
+                    timer.Start();
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        input.CropWholeWords(input.Length - 10);
+                    }
+                    timer.Stop();
+                    elapsedMilliseconds = timer.ElapsedMilliseconds;
+                };
+
+        It should_return_yellow = () =>
+                {
+                    throw new Exception(elapsedMilliseconds.ToString());
+                };
+    }
+}
